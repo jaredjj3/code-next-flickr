05d2bf53d3b02c9c2e49a24f2ea4c73df5d7d5b0
jaredjj3 <jared@jaredjohnson.dev>
2020-11-09 14:00:46 -0500

    1 file modified

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,13 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+import * as FlickrAPI from "../data/FlickrAPI";
 
 export const Config = props => {
   // Get the API key from local storage.
   const [apiKey, setApiKey] = useLocalStorage("FLICKR_API_KEY", "");
+  const [pending, setPending] = useState(false);
+  const [tested, setTested] = useState(false);
+  const [photos, setPhotos] = useState({});
+
+  const testApi = async () => {
+    setPending(true);
+    setTested(false);
+    setPhotos({});
+    try {
+      const photos = await FlickrAPI.search(apiKey, "people", 3);
+      setPhotos(photos);
+    } finally {
+      setPending(false);
+      setTested(true);
+    }
+  };
 
   // Handle input changes.
   const onChange = e => {
     setApiKey(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(apiKey);
+    testApi();
   };
 
   // Allow the parent component to listen to api key changes.
@@ -32,28 +55,60 @@ export const Config = props => {
           </h5>
         </div>
 
-        <div id="config-form" className="collapse" data-parent="#accordion">
+        <div
+          id="config-form"
+          className="collapse show"
+          data-parent="#accordion"
+        >
           <div className="card-body">
-            <div className="form-group">
-              <label for="api-key">Flickr API Key</label>
-              <input
-                type="email"
-                className="form-control"
-                id="api-key"
-                value={apiKey}
-                onChange={onChange}
-              />
-              <small className="form-text text-muted">
-                You can get a new API key or view your existing ones at{" "}
-                <a
-                  target="_blank"
-                  href="https://www.flickr.com/services/apps/create/"
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <label for="api-key">Flickr API Key</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="api-key"
+                  value={apiKey}
+                  onChange={onChange}
+                />
+                <small className="form-text text-muted">
+                  You can get a new API key or view your existing ones at{" "}
+                  <a
+                    target="_blank"
+                    href="https://www.flickr.com/services/apps/create/"
+                  >
+                    Flickr
+                  </a>
+                  .
+                </small>
+              </div>
+
+              <div className="form-group">
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  disabled={pending}
                 >
-                  Flickr
-                </a>
-                .
-              </small>
-            </div>
+                  {pending ? "Testing API..." : "Test API"}
+                </button>
+              </div>
+
+              {tested && (
+                <>
+                  {photos && photos.stat === "ok" ? (
+                    <div className="alert alert-success">
+                      <p>FlickrAPI.search was successful:</p>
+                      <pre>{JSON.stringify(photos, null, 2)}</pre>
+                    </div>
+                  ) : (
+                    <div className="alert alert-warning">
+                      <p>FlickrAPI.search had an issue:</p>
+                      <pre>{JSON.stringify(photos, null, 2)}</pre>
+                    </div>
+                  )}
+                </>
+              )}
+            </form>
           </div>
         </div>
       </div>
