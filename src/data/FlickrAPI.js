8b1a5d17bdc08a16e08636e7231ea82159ba7a88
jaredjj3 <jared@jaredjohnson.dev>
2020-11-16 15:53:55 -0500

    1 file modified

--- src/data/FlickrAPI.js
+++ src/data/FlickrAPI.js
@@ -2,11 +2,17 @@ import { debounce } from 'lodash';
 
 export const FLICKR_API_DEBOUNCED_MS = 1000;
 
+export const FLICKR_API_MAX_LIMIT = 100;
+
 export const FLICKR_API_URL = "https://api.flickr.com/services/rest/";
 
 // We use debounce to prevent people from accidentally spamming this.
 // https://lodash.com/docs/4.17.15#debounce
 export const search = debounce(async (apiKey, tags, limit, onResponse) => {
+  // Prevent from requesting more than FLICKR_API_MAX_LIMIT
+  limit = limit || 1;
+  limit = Math.min(limit, FLICKR_API_MAX_LIMIT);
+
   // https://www.flickr.com/services/api/flickr.photos.search.html
   const url = new URL(FLICKR_API_URL);
   url.searchParams.append("method", "flickr.photos.search");
